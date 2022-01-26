2ab263f576e0b980ac033a8ad07bb2ad591df675
Kenny <kburawudi@yahoo.com>
2022-01-26 23:28:37 +0200

    New web scraper

--- webdriver.py
+++ webdriver.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
+import pandas as pd
 
 driver = webdriver.Chrome()
 driver.get("https://www.nba.com/stats/leaders/?Season=2021-22&SeasonType=Regular%20Season")
@@ -8,3 +9,6 @@ s = driver.find_element(By.CLASS_NAME,"stats-table-pagination")
 s = driver.find_element(By.XPATH,"/html/body/main/div/div/div[2]/div/div/nba-stat-table/div[1]/div/div/select")
 s = Select(s)
 s.select_by_visible_text("All")
+
+html = driver.page_source
+table = pd.read_html(html)[0]
