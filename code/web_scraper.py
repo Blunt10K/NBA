30cdaba841b4c92f7fab365ffb34a3e50c01e541
Kenny <kburawudi@yahoo.com>
2022-01-27 22:41:35 +0200

    Reorganised repository

---
+++ code/web_scraper.py
@@ -0,0 +1,95 @@
+from bs4 import BeautifulSoup
+import re
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+
+class Teams:
+    def __init__(self):
+        self.root = "https://www.nba.com/stats/teams/traditional/?"
+        self.REG = "Regular%20Season"
+        self.POST = "Playoffs"
+        self.mid = "Season=" # +[year] YYYY-YY +
+        self.tail = "&SeasonType=" # + [Regular%20Season|Playoffs]
+#             self.xpath = "/html/body/main/div/div/div[2]/div/div/nba-stat-table/div[2]/div[1]/table"
+
+    def build_url(self,year,reg_season = False):
+        if(reg_season):
+            return self.root + self.mid + year + self.tail + self.REG
+
+        return self.root + self.mid + year + self.tail + self.POST
+
+    def get_team_ids(self,html):
+        soup = BeautifulSoup(html,'html.parser')
+        table = soup.find("table")
+
+        tids = []
+        team_reg = '/team/\d+'
+
+        for l in table.find_all('a'):
+            team_match = re.search(team_reg,l.get('href'))
+
+            if(team_match):
+                team_id = int(re.search('\d+',team_match.group()).group())
+                tids.append(team_id)
+
+        return tids
+
+    def get_source_and_teams(self, url, driver):
+        while(1<2):
+            try:
+                driver.get(url)
+                html = driver.page_source
+                return html, self.get_team_ids(html)
+            except:
+                pass
+
+
+
+class Players:
+    def __init__(self):
+        self.REG = "Regular%20Season"
+        self.POST = "Playoffs"
+        self.root = "https://www.nba.com/stats/players/traditional/?" # + [teamname -prefix_1] +
+        self.mid = "Season=" # +[year] YYYY-YY +
+        self.tail = "&SeasonType=" # + [Regular%20Season|Playoffs]
+        self.xpath = "/html/body/main/div/div/div[2]/div/div/nba-stat-table/div[1]/div/div/select"
+
+    def build_url(self,year,reg_season = False):
+        if(reg_season):
+            return self.root + self.mid + year + self.tail + self.REG
+
+        return self.root + self.mid + year + self.tail + self.POST
+
+    def click_all(self, url, driver):
+        driver.get(url)
+        s = driver.find_element(By.XPATH,self.xpath)
+        s = Select(s)
+        s.select_by_visible_text("All")
+
+        return driver.page_source
+
+
+    def get_player_and_team_ids(self,html):
+
+        soup = BeautifulSoup(html, 'html.parser')
+        table = soup.find("table")
+
+        pids = []
+        tids = []
+
+        player_reg = '/player/\d+'
+        team_reg = '/team/\d+'
+
+        for l in table.find_all('a'):
+            player_match = re.search(player_reg,l.get('href'))
+
+            team_match = re.search(team_reg,l.get('href'))
+
+            if(player_match):
+                player_id = int(re.search('\d+',player_match.group()).group())
+                pids.append(player_id)
+            elif(team_match):
+                team_id = int(re.search('\d+',team_match.group()).group())
+                tids.append(team_id)
+
+        return pids, tids
