b76a32ad8be245f7c10eaaa039be138d0cc7a307
Kenny <kburawudi@yahoo.com>
2022-04-03 13:05:18 +0200

    Edit app layout

--- app.py
+++ app.py
@@ -137,28 +137,30 @@ app.layout = html.Div(style={'backgroundColor':colours['background'],'fontFamily
 						player_stats()
 					 ],
 				style = {'textAlign': 'right',"flex":1}
-			),
-
-			html.br(),
-
-			html.Div(player_slider(),
-				style = {'textAlign': 'center',"flex":1}
-			),
-
-			html.br(),
-			
-			html.Div(dcc.Graph(figure = fig, id = 'graph')
-				# style = {'textAlign': 'right',"flex":1}
-			),
-
-			html.br(),
-
-			html.Div(year_slider(),
-				style = {'textAlign': 'center',"flex":1}
 			)
+
+			
 
 		],style = {'display':'flex','flex-direction': 'row'}
 	),
+
+	html.br(),
+
+	html.Div(player_slider(),
+		style = {'textAlign': 'center',"flex":1}
+	),
+
+	html.br(),
+
+	html.Div(dcc.Graph(figure = fig, id = 'graph')
+		# style = {'textAlign': 'right',"flex":1}
+	),
+
+	html.br(),
+
+	html.Div(year_slider(),
+		style = {'textAlign': 'center',"flex":1}
+	)
 ])
 
 @app.callback(
