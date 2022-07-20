03b92f58d015abca6e31bc7ad923851b345a5203
Kenny <kburawudi@yahoo.com>
2022-07-20 21:01:23 +0200

    cleanup

--- notebooks/code/db_utils.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-
-def make_engine(user, pswd, db):
-
-    return create_engine("mariadb+mariadbconnector://"\
-                        +user+":"\
-                        +pswd+"@127.0.0.1:3306/"+db)
--- notebooks/code/test.py
+++
@@ -1,7 +0,0 @@
-from extract import player_boxes
-
-# j= 0 
-# for i in player_boxes():
-#     with open('page_'+''+str(i)+'.html','w') as f:
-#         f.write(f)
-#     j+=1
