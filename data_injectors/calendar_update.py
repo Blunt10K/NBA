3e1a6744a53b44be33931376f934c53425f84cb8
Kenny Burawudi <kburawudi@yahoo.com>
2023-12-10 22:31:32 +0100

    add calenday update code

--- data_injectors/calendar_update.py
+++ data_injectors/calendar_update.py
@@ -9,7 +9,8 @@ from airflow.models import Variable
 def make_engine(host, port, user, pswd, db):
 
     return create_engine(f"postgresql+psycopg2://{user}:{pswd}@{host}:{port}/{db}")
-                         
+
+
 def get_quarter(p_date: date_class) -> int:
     return (p_date.month - 1) // 3 + 1
 
@@ -22,22 +23,29 @@ def get_last_day_of_the_quarter(p_date: date_class):
     quarter = get_quarter(p_date)
     return datetime(p_date.year + 3 * quarter // 12, 3 * quarter % 12 + 1, 1) + timedelta(days=-1)
 # %%
-df = pd.DataFrame(data=dict(game_date = [datetime.today()],
-                            quarter_from = [get_first_day_of_the_quarter(datetime.today())],
-                            quarter_to = [get_last_day_of_the_quarter(datetime.today())]))
-# print(get_first_day_of_the_quarter(datetime.now()),get_last_day_of_the_quarter(datetime.now()))
-# %%
-df
-# %%
-host = Variable.get('HOSTNAME')
-db = Variable.get('NBA_DB')
-port = Variable.get('PORT')
-user = Variable.get('USER')
-pswd = Variable.get('PSWD!')
 
-conn = make_engine(host, port, user, pswd, db)
-# %%
-conn
-# %%
-df.to_sql('test_calendar', conn, if_exists = 'append', index = False)#, schema = str(schema))
-# %%
+def create_connector():
+    host = Variable.get('HOSTNAME')
+    db = Variable.get('NBA_DB')
+    port = Variable.get('PORT')
+    user = Variable.get('USER')
+    pswd = Variable.get('PSWD!')
+
+    conn = make_engine(host, port, user, pswd, db)
+    
+    return conn
+
+
+def create_record():
+    conn = create_connector()
+    table = 'calendar'
+    df = pd.DataFrame(data=dict(game_date = [datetime.today()],
+                                quarter_from = [get_first_day_of_the_quarter(datetime.today())],
+                                quarter_to = [get_last_day_of_the_quarter(datetime.today())]))
+    
+    
+    df.to_sql(table, conn, if_exists = 'append', index = False)
+
+    conn.dispose()
+
+    return
---
+++ data_injectors/calendar_update_dag.py
@@ -0,0 +1,22 @@
+# %%
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+import pendulum
+import sys
+from os.path import expanduser,join as osjoin
+
+# %%
+with DAG('add_',default_args={'retries': 4,'owner':'blunt10k'},description='Data injector to calendar table',
+schedule_interval='0 0 * * *',catchup=True,tags=['nba_stats'],
+start_date=pendulum.datetime(2023, 12, 11, tz="UTC")) as dag:
+
+    dag.doc_md = __doc__
+
+    code_directory = expanduser(osjoin('~/airflow','dags','data_injectors'))
+    sys.path.insert(0,code_directory)
+
+    from calendar_update import create_record
+
+    update_calendar = PythonOperator(task_id = 'inject_date',python_callable=create_record)
+
+    update_calendar
