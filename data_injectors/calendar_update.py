ae0c386e3f2e4617a5f5b698cc98c06584ea2a58
Kenny Burawudi <kburawudi@yahoo.com>
2023-12-10 15:44:33 +0100

    add a data injector code

---
+++ data_injectors/calendar_update.py
@@ -0,0 +1,43 @@
+# %%
+from datetime import date as date_class
+from datetime import timedelta, datetime
+import pandas as pd
+from sqlalchemy import create_engine
+from airflow.models import Variable
+
+
+def make_engine(host, port, user, pswd, db):
+
+    return create_engine(f"postgresql+psycopg2://{user}:{pswd}@{host}:{port}/{db}")
+                         
+def get_quarter(p_date: date_class) -> int:
+    return (p_date.month - 1) // 3 + 1
+
+
+def get_first_day_of_the_quarter(p_date: date_class):
+    return datetime(p_date.year, 3 * ((p_date.month - 1) // 3) + 1, 1)
+
+
+def get_last_day_of_the_quarter(p_date: date_class):
+    quarter = get_quarter(p_date)
+    return datetime(p_date.year + 3 * quarter // 12, 3 * quarter % 12 + 1, 1) + timedelta(days=-1)
+# %%
+df = pd.DataFrame(data=dict(game_date = [datetime.today()],
+                            quarter_from = [get_first_day_of_the_quarter(datetime.today())],
+                            quarter_to = [get_last_day_of_the_quarter(datetime.today())]))
+# print(get_first_day_of_the_quarter(datetime.now()),get_last_day_of_the_quarter(datetime.now()))
+# %%
+df
+# %%
+host = Variable.get('HOSTNAME')
+db = Variable.get('NBA_DB')
+port = Variable.get('PORT')
+user = Variable.get('USER')
+pswd = Variable.get('PSWD!')
+
+conn = make_engine(host, port, user, pswd, db)
+# %%
+conn
+# %%
+df.to_sql('test_calendar', conn, if_exists = 'append', index = False)#, schema = str(schema))
+# %%
