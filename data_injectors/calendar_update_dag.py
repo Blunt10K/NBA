d12e6948cb68367633338826012390ef3820e75c
Kenny Burawudi <kburawudi@yahoo.com>
2023-12-10 23:04:40 +0100

    remove catch up from calendar update

--- data_injectors/calendar_update_dag.py
+++ data_injectors/calendar_update_dag.py
@@ -7,7 +7,7 @@ from os.path import expanduser,join as osjoin
 
 # %%
 with DAG('add_dates_to_calendar',default_args={'retries': 4,'owner':'blunt10k'},description='Data injector to calendar table',
-schedule_interval='0 0 * * *',catchup=True,tags=['nba_stats'],
+schedule_interval='0 0 * * *',catchup=False,tags=['nba_stats'],
 start_date=pendulum.datetime(2023, 12, 11, tz="UTC")) as dag:
 
     dag.doc_md = __doc__
