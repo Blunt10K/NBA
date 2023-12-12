dab1a633ee2d60ad481fb0daa63c94cc8b07d2f6
Kenny Burawudi <kburawudi@yahoo.com>
2023-12-12 21:29:08 +0100

    sfdvga

--- scraper/crawler.py
+++ scraper/crawler.py
@@ -8,48 +8,51 @@ from sqlalchemy import create_engine
 from airflow.models import Variable
 
 
-def make_engine():
-    host = Variable.get('HOSTNAME')
-    db = Variable.get('NBA_DB')
-    port = Variable.get('PORT')
-    user = Variable.get('USER')
-    pswd = Variable.get('PSWD')
-
-    return create_engine(f"postgresql+psycopg2://{user}:{pswd}@{host}:{port}/{db}")
-
-
-def game_dates():
-    engine = make_engine()
-    latest_scrape = pd.read_sql('SELECT max(game_date) as latest from scraped_games', engine)
-    latest_scrape = latest_scrape.loc[0,'latest']
-
-    if latest_scrape:
-        query = f'''SELECT game_date from calendar
-        where (game_date > to_date({latest_scrape.strftime('%Y-%m-%d')},'YYYY-MM-DD'))
-        AND (to_date({(latest_scrape + td(1)).strftime('%Y-%m-%d')},'YYYY-MM-DD') between quarter_from and quarter_to)'''
-    else:
-        query = f'''SELECT game_date from calendar
-        where (game_date > to_date('1996-11-01','YYYY-MM-DD'))
-        AND (to_date('1996-11-01','YYYY-MM-DD') between quarter_from and quarter_to)'''
-
-    df = pd.read_sql(query, engine)
-
-    engine.dispose()
-
-    for i in df.itertuples():
-        yield 'https://www.nba.com/games?date='+dt.strftime(i.game_date, '%Y-%m-%d')
-
-
 class GamesSpider(CrawlSpider):
-    name = 'pbp-games'
-    allowed_domains = ['nba.com']
-    start_urls = list(game_dates())
-    REDIRECT_ENABLED = False
-    # extract_path = join(Variable.get('EXTDISK'),'spark_apps','games','data.json')
 
-    # custom_settings = dict(FEEDS = {extract_path: {'format':'jsonl','overwrite':False}})
+    def __init__(self, *a, **kw):
+        # super().__init__(*a, **kw)
+        self.name = 'pbp-games'
+        self.allowed_domains = ['nba.com']
+        self.start_urls = list(self.game_dates())
+        self.REDIRECT_ENABLED = False
+        # extract_path = join(Variable.get('EXTDISK'),'spark_apps','games','data.json')
 
-    rules = [Rule(LinkExtractor(allow=['\w+-vs-\w+-\d+/box-score#box-score']), callback='parse_page')]
+        # custom_settings = dict(FEEDS = {extract_path: {'format':'jsonl','overwrite':False}})
+
+        self.rules = [Rule(LinkExtractor(allow=['\w+-vs-\w+-\d+/box-score#box-score']), callback=self.parse_page)]
+    
+    @staticmethod
+    def make_engine(self):
+        host = Variable.get('HOSTNAME')
+        db = Variable.get('NBA_DB')
+        port = Variable.get('PORT')
+        user = Variable.get('USER')
+        pswd = Variable.get('PSWD')
+
+        return create_engine(f"postgresql+psycopg2://{user}:{pswd}@{host}:{port}/{db}")
+
+    @staticmethod
+    def game_dates(self):
+        engine = self.make_engine()
+        latest_scrape = pd.read_sql('SELECT max(game_date) as latest from scraped_games', engine)
+        latest_scrape = latest_scrape.loc[0,'latest']
+
+        if latest_scrape:
+            query = f'''SELECT game_date from calendar
+            where (game_date > to_date({latest_scrape.strftime('%Y-%m-%d')},'YYYY-MM-DD'))
+            AND (to_date({(latest_scrape + td(1)).strftime('%Y-%m-%d')},'YYYY-MM-DD') between quarter_from and quarter_to)'''
+        else:
+            query = f'''SELECT game_date from calendar
+            where (game_date > to_date('1996-11-01','YYYY-MM-DD'))
+            AND (to_date('1996-11-01','YYYY-MM-DD') between quarter_from and quarter_to)'''
+
+        df = pd.read_sql(query, engine)
+
+        engine.dispose()
+
+        for i in df.itertuples():
+            yield 'https://www.nba.com/games?date='+dt.strftime(i.game_date, '%Y-%m-%d')
 
     def parse_page(self, response):
         items = response.css('script[type="application/json"]::text')
