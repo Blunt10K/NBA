e9c016ec8b5312a90653d7e5211b1c23e98ac203
Kenny Burawudi <kburawudi@yahoo.com>
2023-12-17 22:40:34 +0100

    add cwd

--- scraper/dag.py
+++ scraper/dag.py
@@ -15,6 +15,8 @@ start_date=pendulum.datetime(2023, 12, 10, tz="UTC")) as dag:
 
     code_directory = expanduser(osjoin('~/airflow','dags','NBA','scraper'))
     sys.path.insert(0,code_directory)
+    cwd = expanduser(osjoin('~/airflow','dags','NBA','scraper'))
+
 
     from get_scrape_data import write_to_db
 
@@ -22,8 +24,10 @@ start_date=pendulum.datetime(2023, 12, 10, tz="UTC")) as dag:
     def scrape_logs_to_db():
         write_to_db()
 
-    scrape_games = BashOperator(task_id="scrape_pbp",bash_command="bash scrape_games.sh ",cwd='.')
-    failed_scrapes = BashOperator(task_id="get_failed_scrapes",bash_command="bash failed_scrapes.sh ",cwd='.')
+    
+
+    scrape_games = BashOperator(task_id="scrape_pbp",bash_command="bash scrape_games.sh ",cwd=cwd)
+    failed_scrapes = BashOperator(task_id="get_failed_scrapes",bash_command="bash failed_scrapes.sh ",cwd=cwd)
     load_logs = scrape_logs_to_db()
 
     scrape_games >> failed_scrapes >> load_logs
