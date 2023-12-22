4f69703c32d1564806146e3c16ba3aa29b73a575
Kenny Burawudi <kburawudi@yahoo.com>
2023-12-22 16:13:54 +0100

    add calendar update functionality

--- scraper/dag.py
+++ scraper/dag.py
@@ -19,15 +19,20 @@ start_date=pendulum.datetime(2023, 12, 10, tz="UTC")) as dag:
 
 
     from get_scrape_data import write_to_db
+    from db_utils import update_calendar
 
     @task(task_id="write_scrape_logs_to_db")
     def scrape_logs_to_db():
         write_to_db()
-
     
+    @task(task_id="update_calendar")
+    def update_calendar_func():
+        update_calendar()
+
 
     scrape_games = BashOperator(task_id="scrape_pbp",bash_command="bash scrape_games.sh ",cwd=cwd)
     failed_scrapes = BashOperator(task_id="get_failed_scrapes",bash_command="bash failed_games.sh ",cwd=cwd)
     load_logs = scrape_logs_to_db()
+    update_scraped_games = update_calendar_func()
 
-    scrape_games >> load_logs >> failed_scrapes
+    scrape_games >> load_logs >> failed_scrapes >> update_scraped_games
--- scraper/db_utils.py
+++ scraper/db_utils.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from airflow.models import Variable
+from datetime import timedelta as td
 
 def make_engine():
     host = Variable.get('HOSTNAME')
@@ -9,3 +10,70 @@ def make_engine():
     pswd = Variable.get('PSWD')
 
     return create_engine(f"postgresql+psycopg2://{user}:{pswd}@{host}:{port}/{db}")
+
+def get_latest(connection):
+
+    latest = '''SELECT max(game_date) from calendar 
+            where scraped = TRUE'''
+
+    t = text(latest)
+    latest_scrape = connection.execute(t).first()[0]
+
+    return latest_scrape
+
+
+def get_bounds(connection, latest_scrape):
+    query = f'''SELECT min(game_date), max(game_date) from calendar
+            where
+            (to_date('{(latest_scrape + td(30)).strftime('%Y-%m-%d')}',
+                'YYYY-MM-DD')
+                between quarter_from and quarter_to
+            ) and
+            game_date > to_date(
+                '{(latest_scrape).strftime('%Y-%m-%d')}','YYYY-MM-DD')
+            '''
+
+    t = text(query)
+
+    dates = connection.execute(t)
+    min_date, max_date = dates.first()
+
+    return min_date, max_date
+
+
+def update_query(min_date, max_date):
+
+    lower_bound = f'''to_date('{min_date.strftime('%Y-%m-%d')}',
+                'YYYY-MM-DD')'''
+    
+    upper_bound = f'''to_date('{max_date.strftime('%Y-%m-%d')}',
+                'YYYY-MM-DD')'''
+
+    update = f'''UPDATE calendar
+                SET scraped = True
+                where
+                game_date between {lower_bound} and {upper_bound}
+            '''
+    
+    return update
+
+def update_calendar():
+
+    conn = make_engine()
+
+    
+    connection = conn.connect()
+
+    latest_scrape = get_latest(connection)
+
+    min_date, max_date = get_bounds(connection, latest_scrape)
+
+
+    t = text(update_query(min_date, max_date))
+    
+    connection.execute(t)
+    connection.commit()
+
+    connection.close()
+
+    return
